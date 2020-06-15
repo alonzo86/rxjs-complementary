a640a085f7f36f6835133e2b90593b101772a348
AlonYampolski <alon.yampolski@n2ws.com>
2020-06-15 16:16:14 +0300

    setup coverage reporting

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,15 @@
+const path = require('path');
+
 module.exports = (config) => {
     config.set({
         basePath: '',
-        frameworks: ['jasmine', "karma-typescript"],
+        frameworks: ['jasmine', 'karma-typescript'],
         plugins: [
             require('karma-jasmine'),
+            require('karma-coverage'),
             require('karma-chrome-launcher'),
             require('karma-typescript'),
-            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter')
         ],
         client: {
             clearContext: false, // leave Jasmine Spec Runner output visible in browser
@@ -14,12 +17,12 @@ module.exports = (config) => {
                 random: false
             }
         },
-        reporters: ['progress', 'kjhtml'],
+        reporters: ['progress', 'karma-typescript', 'coverage-istanbul'],
         port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+        browsers: ['ChromeHeadlessNoSandbox'],
         customLaunchers: {
           ChromeHeadlessNoSandbox: {
             base: 'ChromeHeadless',
@@ -31,7 +34,7 @@ module.exports = (config) => {
             'src/**/*.ts'
         ],
         preprocessors: {
-            '**/*.ts': 'karma-typescript'
+            '**/*.ts': ['karma-typescript']
         },
         karmaTypescriptConfig: {
             compilerOptions: {
@@ -39,5 +42,17 @@ module.exports = (config) => {
             },
             tsconfig: './tsconfig.json',
         },
+        coverageIstanbulReporter: {
+            reports: ['html', 'lcovonly', 'text-summary'],
+            dir: path.join(__dirname, 'coverage'),
+            combineBrowserReports: true,
+            fixWebpackSourcePaths: true,
+            skipFilesWithNoCoverage: true,
+            'report-config': {
+                html: {
+                    subdir: 'html'
+                }
+            }
+        }
     });
 };
